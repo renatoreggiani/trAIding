3ae3e456e8dc201cdf7a4bf79487ac97fed349f0
Pedro Henrique Lomnitzer <pedro_lomnitzer@hotmail.com>
2021-03-17 19:57:13 -0300

    arima model ajusta, fit, salva e forecast

--- modelo_arima.py
+++ modelo_arima.py
@@ -5,6 +5,7 @@ for model in modelos:
         m = eval(model)
         m = m(df, ticker)
         m.atualiza(daysfor=2)
+        m.salva_modelo
 
 # validador tudo
 for model in modelos:
@@ -54,17 +55,17 @@ class modelo_arima(AbstractModelo):
         # exclui todas as colunas exceto data e y
         self.df.dropna(subset=['y'], inplace=True)
         for col in self.df.columns:
-            if col not in ['date', 'y']:
+            if col not in ['y']:
                 self.df.drop(col, axis='columns', inplace=True)
 
     def fit(self) -> None:
-        is_stat = stationary_test(self.df)
-        arima_model = auto_arima(self.df, stationary=is_stat, start_p=0, start_d=0, start_q=0,
+        is_stat = stationary_test(self.df['y'])
+        arima_model = auto_arima(self.df['y'], stationary=is_stat, start_p=0, start_d=0, start_q=0,
                                  max_p=10, max_d=10, max_q=10, start_P=0, start_D=0, start_Q=0,
                                  max_P=15, max_D=15, max_Q=15, m=15, seasonal='False', n_fits=50,
                                  error_action='warn', trace=True, suppress_warnings=True,
                                  stepwise=False, random_state=20, n_jobs=-1)
-        self.modelo = arima_model.fit(disp=0)
+        self.modelo = arima_model.fit(y=self.df['y'])
 
     def salva_modelo(self):
         try:
@@ -73,13 +74,15 @@ class modelo_arima(AbstractModelo):
         except FileNotFoundError:
             jdata = {}
         hoje = datetime.datetime.now().strftime("%Y-%m-%d")
-        jdata[self.ticker].update({"parametros": self.modelo.order + self.modelo.seasonal_order,
-                                   "train_date": hoje})
+        if self.ticker not in jdata:
+            jdata[self.ticker] = {}
+        jdata[self.ticker]['parametros'] = self.modelo.order + self.modelo.seasonal_order
+        jdata[self.ticker]['train_date'] = hoje
         json.dump(jdata, open('modelos/arima/arima.json', 'w'), indent=4)
-        pickle.dump(self.modelo, open("modelos/arima/arima."+self.ticker+".bin", "wb"))
+        pickle.dump(self.modelo, open("modelos/arima/arima_"+self.ticker, "wb"))
 
     def forecast(self) -> float:
-        self.modelo.forecast()[0]
+        self.modelo.predict(n_periods=1, alpha=0.05)[0]
 
     def atualiza_modelo(self):
         train_date = datetime.datetime.strptime(os.path.getmtime("modelos/arima/arima." +
@@ -89,3 +92,18 @@ class modelo_arima(AbstractModelo):
 
     def carrega_modelo(self):
         self.modelo = pickle.load(open("modelos/arima/arima."+self.ticker+".bin", 'rb'))
+
+
+# In[]
+
+import yfinance as yf
+ticker = 'BBAS3.SA'
+tkr = yf.Ticker(ticker)
+df = tkr.history(period='1y', interval='1d')
+df = df.rename(columns={'Close': 'y'}, inplace=False)
+
+m = modelo_arima(df, ticker)
+m.ajusta_dados()
+m.fit()
+m.salva_modelo()
+f = m.forecast()
