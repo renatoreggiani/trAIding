b0cbb740c3f1a621a065bf7ac55d2e0127025e0b
renatoreggiani <renatoreggiani@gmail.com>
2021-04-02 17:41:28 -0300

    Update teste_uni8co.py

--- teste_uni8co.py
+++ teste_uni8co.py
@@ -18,11 +18,11 @@ import datetime
 
 ticker = 'BBAS3.SA'
 tkr = yf.Ticker(ticker)
-df = tkr.history(period='1y', interval='1d')
+df = tkr.history(period='5y', interval='1d')
 df = df.rename(columns={'Close': 'y'}, inplace=False)
 series = TimeSeries.from_dataframe(df, time_col=None, value_cols='y', freq='D')
-t = time.mktime(datetime.datetime.strptime('2020-05-01', "%Y-%m-%d").timetuple())
-train, val = series.split_after(pd.Timestamp(t))  # erro fdp!
+#t = time.mktime(datetime.datetime.strptime('2020-06-01', "%Y-%m-%d").timetuple())
+train, val = series.split_after(pd.Timestamp('2017-01-01'))  # erro era culpa da linha acima!
 
 model = ExponentialSmoothing()
 model.fit(train)
