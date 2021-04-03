56c17baa721915e1e294585ac87efc90b22a2841
Pedro Henrique Lomnitzer <pedro_lomnitzer@hotmail.com>
2021-04-03 12:31:04 -0300

    Update teste_uni8co.py

--- teste_uni8co.py
+++ teste_uni8co.py
@@ -10,8 +10,7 @@ import yfinance as yf
 import pandas as pd
 from darts.models import ExponentialSmoothing
 from darts import TimeSeries
-import time
-import datetime
+import matplotlib.pyplot as plt
 
 
 # In[]
@@ -21,9 +20,13 @@ tkr = yf.Ticker(ticker)
 df = tkr.history(period='5y', interval='1d')
 df = df.rename(columns={'Close': 'y'}, inplace=False)
 series = TimeSeries.from_dataframe(df, time_col=None, value_cols='y', freq='D')
-#t = time.mktime(datetime.datetime.strptime('2020-06-01', "%Y-%m-%d").timetuple())
-train, val = series.split_after(pd.Timestamp('2017-01-01'))  # erro era culpa da linha acima!
+train, val = series.split_after(pd.Timestamp('2020-05-02'))
 
 model = ExponentialSmoothing()
 model.fit(train)
 prediction = model.predict(len(val))
+
+series.plot(label='actual')
+prediction.plot(label='forecast')
+plt.legend()
+plt.xlabel('Date')
