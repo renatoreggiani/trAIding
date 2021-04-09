3ac12821d0ea39d49f5444ef775d0ef69f92ca92
renatoreggiani <renatoreggiani@gmail.com>
2021-04-09 19:00:08 -0300

    Update modelo_prophet.py

--- modelo_prophet.py
+++ modelo_prophet.py
@@ -20,6 +20,7 @@ import os
 from interfaces import AbstractModelo
 from functions import get_finance_data
 
+
 # %%
 
 
@@ -34,30 +35,37 @@ class ModeloProphet(AbstractModelo):
         self.ticker = ticker
         self.__nome_modelo = self.nome_modelo
         self.__binfilename = 'modelos/PROPHET_' + self.ticker + '.bin'
-        self.df = df.copy()
-        self.periods = periods
-        self.df_prophet = self.ajusta_dados()
+        self.lag_regressors = [col for col in df.columns if col.startswith('x_')]
+        self.df, self.df_regressors = self.ajusta_dados(df)
 
-        # self.forecast = self.forecast()
 
     @property
     def nome_modelo(self) -> str:
         return f'modelo_prophet_{self.ticker}'
 
-    def ajusta_dados(self):
-        # df_prophet = self.df.copy()
-        # df_prophet = df_prophet[['ds', 'y']].sort_values('ds')
-        return self.df.dropna()
+    def ajusta_dados(self, df):
+        df_regressors = df[[col for col in df.columns if col.startswith('x_')]]
+        df = df.copy()[['y', 'ds']]
+        if not df_regressors.empty:
+            df = df.merge(df_regressors.shift(1), left_index=True, right_index=True)
+            df_regressors = df_regressors.shift(1, freq='B')
+            df_regressors.loc[:,'ds'] = df_regressors.index.values
+        return df.dropna(), df_regressors
 
     def fit(self):
         self.modelo = Prophet(growth='linear', daily_seasonality=False,
                               yearly_seasonality='auto', n_changepoints=140,
                               weekly_seasonality='auto', seasonality_mode='multiplicative',
                               seasonality_prior_scale=100)
-        self.modelo.fit(self.df_prophet)
+        for reg in self.lag_regressors:
+            self.modelo.add_regressor(reg)
+        self.modelo.fit(self.df)
 
     def forecast(self, periods=1, plot=False):
         self.future = self.modelo.make_future_dataframe(periods=periods, freq='B')
+        if not self.df_regressors.empty:
+            self.future = self.future.merge(self.df_regressors, left_on='ds', right_on='ds')
+
         if plot:
             return self.modelo.predict(self.future)
         else:
@@ -96,12 +104,44 @@ class ModeloProphet(AbstractModelo):
 
 
 # %% teste
-ticker = 'ITUB4.SA'
-df = get_finance_data(ticker, period='1y')
-# df.y = df.y.pct_change()
-m = ModeloProphet(df, ticker)
-m.fit()
-m.plot()
-m.plot_sazonalidade()
-m.forecast()
 
+if __name__ == '__main__':
+    ticker = 'ITUB4.SA'
+    df = get_finance_data(ticker, period='10y')
+    # df.y = df.y.pct_change()
+    df['x_volume'] = df.loc[:,['Volume']].shift(1, freq='B')
+    df.merge(df_reg, right_index=True, left_index=True, how='outer')
+    m = ModeloProphet(df, ticker)
+    m.fit()
+    m.plot()
+    m.plot_sazonalidade()
+    m.forecast()
+
+
+
+m = Prophet()
+m.add_country_holidays(country_name='BR')
+m.fit(df)
+
+
+import holidays
+feriados= holidays.Brazil()
+for feriado in feriados['2020-01-01': '2020-12-31'] :
+    print(feriado)
+
+
+
+
+
+import pandas_market_calendars as mcal
+
+
+
+
+print(mcal.get_calendar_names())
+
+
+
+mkdays = mcal.exchange_calendar_bmf.BMFExchangeCalendar()
+early = mkdays.schedule(start_date=df.index[1].strftime('%Y-%m-%d'), end_date=df.index[-1].strftime('%Y-%m-%d'))
+mcal.date_range(early, frequency='1D')
